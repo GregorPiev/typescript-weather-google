73514d94418aa9eb9af67b80b383d1a20f38ec6c
GregorPiev <gregpiev@gmail.com>
2021-09-12 15:34:01 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-jjywvf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-jjywvf)
---
+++ src/CustomMap.ts
@@ -0,0 +1,64 @@
+import * as google from 'google-maps';
+
+interface IMappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
+
+export class CustomMap {
+  private googleCustomMap: globalThis.google.maps.Map<HTMLElement>;
+  private googleCreator: any;
+
+  constructor(private mapDivId: string) {
+    this.initMap();
+  }
+
+  initMap(): void {
+    // setTimeout(() => {
+    const loader = new google.Loader(
+      'AIzaSyBh_ohS4Ihf9k415RUqPNAJ6N7AMPIfiT8',
+      {}
+    );
+    loader
+      .load()
+      .then(val => {
+        this.googleCreator = val;
+
+        this.googleCustomMap = new val.maps.Map<HTMLElement>(
+          document.getElementById(this.mapDivId) as HTMLElement,
+          {
+            zoom: 1,
+            center: {
+              lat: 0,
+              lng: 0
+            }
+          }
+        );
+      })
+      .catch(err => {
+        console.log('err:', err.message);
+      });
+    // }, 40);
+  }
+
+  addMarker(mappable: IMappable): void {
+    setTimeout(() => {
+      const marker = new this.googleCreator.maps.Marker({
+        map: this.googleCustomMap,
+        position: {
+          lat: mappable.location.lat,
+          lng: mappable.location.lng
+        }
+      });
+      marker.addListener('click', () => {
+        const infoWindow = new this.googleCreator.maps.InfoWindow({
+          content: 'שלום גרגורי'
+        });
+
+        infoWindow.open(this.googleCustomMap, marker);
+      });
+    }, 1000);
+  }
+}
